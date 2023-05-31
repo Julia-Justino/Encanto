f56ee81d26576a79a21e10441cb9b27b8a4266fe
Julia Justino <stephany.justino@sptech.school>
2023-05-31 13:36:32 -0300

    Add files via upload

--- Web-Data-Viz-My/site/app.js
+++ Web-Data-Viz-My/site/app.js
@@ -13,6 +13,7 @@ var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var contatoRouter = require("./src/routes/contato");
+var pesquisaRouter = require("./src/routes/pesquisa");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +26,7 @@ app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/contato", contatoRouter);
+app.use("/pesquisa", pesquisaRouter);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
